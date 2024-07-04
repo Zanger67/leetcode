c1d4069cbd821ab6e68a12d0f039e0bd3e08fd94
Zanger67 <anthonyzang101@gmail.com>
2024-07-03 19:44:42 -0700

    Reconfiguring markdown generation - test for github markdown display

--- my-submissions/m7.py
+++ my-submissions/m7.py
@@ -1,22 +1,4 @@
 class Solution:
-    ''' Thought Process
-        32-bit integer stored in 2s-comp
-        1000 -> -8
-
-        1110 -> -2
-        1111 -> -1
-        0000 -> 0
-        0001 -> 1
-        0010 -> 2
-        0011 -> 3
-        0100 -> 4
-        0101 -> 5
-        0110 -> 6
-        0111 -> 7
-
-        I feel like there's a way here but I can't find it. Or I might just be
-        overthinking this tbh.
-    '''
     def reverse(self, x: int) -> int:
         val = abs(x)
         output = 0
