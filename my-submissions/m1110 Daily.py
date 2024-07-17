2b2a4d459b2dcc918e34cfcb2df3258773775426
Zanger67 <anthonyzang101@gmail.com>
2024-07-16 17:15:14 -0700

    Daily

---
+++ my-submissions/m1110 Daily.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        output = []
+
+        to_delete = set(to_delete)
+
+        # Returns pointer --> if remove then return null
+        def dfs(curr: Optional[TreeNode], to_delete: set, output: List[Optional[TreeNode]]) -> Optional[TreeNode] :
+            if not curr :
+                return None
+
+            if curr.val in to_delete :
+                left = dfs(curr.left, to_delete, output)
+                right = dfs(curr.right, to_delete, output)
+
+                if left :
+                    output.append(left)
+                if right :
+                    output.append(right)
+
+                return None
+
+            curr.left = dfs(curr.left, to_delete, output)
+            curr.right = dfs(curr.right, to_delete, output)
+            return curr
+
+        head = dfs(root, to_delete, output)
+        if head :
+            output.append(head)
+
+        return output
