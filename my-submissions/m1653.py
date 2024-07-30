2f189a2d063de4b60654697a7ab808737198df65
Zanger67 <anthonyzang101@gmail.com>
2024-07-29 17:29:50 -0700

    daily

---
+++ my-submissions/m1653.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        s = deque(s)
+        counter = 0
+        
+        cnt = Counter(s)
+
+        while s :            
+            if s[0] == 'a' :
+                s.popleft()
+                cnt['a'] -= 1
+                continue
+            if s[-1] == 'b' :
+                s.pop()
+                cnt['b'] -= 1
+                continue
+
+            # which is blocking the least
+            counter += 1
+            if cnt['a'] > cnt['b'] :
+                cnt['b'] -= 1
+                s.popleft()
+            else :
+                cnt['a'] -= 1
+                s.pop()
+
+        return counter
