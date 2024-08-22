c4d2b811089f3cf31db388c1a6d8d7de3ebcf696
Anthony Zang <36343598+Zanger67@users.noreply.github.com>
2024-08-22 15:07:56 -0400

    Create m320.py Weekly Prem

---
+++ my-submissions/m320.py
@@ -0,0 +1,38 @@
+class Solution:
+    def generateAbbreviations(self, word: str) -> List[str]:
+        self.output = []
+
+        def dfs(curr: List[str] = [],
+                currIndx: int = 0,
+                currNum: int = 0,
+                output: List[str] = []) -> List[str] :
+
+            if currIndx >= len(word) :
+                output.append(''.join(curr) + ('' if currNum <= 0 else str(currNum)))
+                return output
+
+            # we have numbers in storage
+            if currNum > 0 :
+                # include number NOW
+                curr.append(str(currNum))       # add number
+                curr.append(word[currIndx])     # add current letter as separator
+                dfs(curr, currIndx + 1, 0, output)
+                curr.pop()                      # pop for space reuse
+                curr.pop()                      # pop for space reuse
+
+                # wait on number by just adding to existing count
+                dfs(curr, currIndx + 1, currNum + 1, output)
+
+            # we have no numbers in storage
+            else :
+                # set current letter as start of abbreviated section
+                dfs(curr, currIndx + 1, 1, output)
+
+                # put in as a letter
+                curr.append(word[currIndx])
+                dfs(curr, currIndx + 1, 0, output)
+                curr.pop()                      # pop for space reuse
+
+            return output
+
+        return dfs()
