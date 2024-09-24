5634819a5fb332de6b463ab83f75820de3a47b48
Anthony Zang <36343598+Zanger67@users.noreply.github.com>
2024-09-24 10:03:28 -0400

    Create e559.py

---
+++ my-submissions/e559.py
@@ -0,0 +1,15 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root :
+            return 0
+        if not root.children :
+            return 1
+        return max(self.maxDepth(x) for x in root.children) + 1
