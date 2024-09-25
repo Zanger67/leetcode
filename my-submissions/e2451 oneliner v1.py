9d819d835d9dc1b445439cd1431fc3ad296c734d
Anthony Zang <36343598+Zanger67@users.noreply.github.com>
2024-09-25 00:04:35 -0400

    Create e2451 oneliner v1.py

---
+++ my-submissions/e2451 oneliner v1.py
@@ -0,0 +1,19 @@
+# Extremly inefficient but one liners are funny lol
+
+class Solution:
+    def oddString(self, words: List[str]) -> str:
+        return {
+            tuple(ord(word[i]) - ord(word[i - 1]) 
+                  for i in range(1, len(word)))
+            :
+            word 
+            for word in words
+        }[
+            sorted(list(Counter(tuple(ord(word[i]) - ord(word[i - 1]) 
+                                      for i in range(1, len(word))) 
+                                      for word in words)), 
+                   key=lambda x: Counter(tuple(ord(word[i]) - ord(word[i - 1]) 
+                                               for i in range(1, len(word))) 
+                                               for word in words)[x])
+            [0]
+        ]
