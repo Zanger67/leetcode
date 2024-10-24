4f1b1976b0158e85e304cf4ef680850d7c36bc36
Anthony Zang <36343598+Zanger67@users.noreply.github.com>
2024-10-23 22:20:03 -0400

    Create m951 oneliner.py

---
+++ my-submissions/m951 oneliner.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def equiv(left: Optional[TreeNode], right: Optional[TreeNode]) -> bool :
+            return False if ((not left or not right) and (left or right)) or (left and right and left.val != right.val) else (not left and not right) or (equiv(left.left, right.left) and equiv(left.right, right.right)) or (equiv(left.right, right.left) and equiv(left.left, right.right))
+
+        return equiv(root1, root2)
