4fb6c3fbdecb5b3103e60a422799b36ba97d5680
Anthony Zang <anthonyzang101@gmail.com>
2025-02-12 01:32:00 -0500

    Designed oneliners for the daily

---
+++ my-submissions/m2342 v2 ONELINER.py
@@ -0,0 +1,3 @@
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        return max([-1] + [sum(heapq.nlargest(2, [-inf] + list(group[1]))) for group in groupby(sorted(nums, key=lambda y: sum(map(int, str(y)))), key=lambda x: sum(map(int, str(x))))])
