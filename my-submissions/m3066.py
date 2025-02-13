e1d89f5260b0ce6c1aafdced74d53212e28b1e8b
Anthony Zang <anthonyzang101@gmail.com>
2025-02-12 19:23:03 -0500

    daily

---
+++ my-submissions/m3066.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        ops = 0
+
+        while len(nums) >= 2 :
+            a, b = heappop(nums), heappop(nums)
+
+            if a >= k and b >= k :
+                return ops
+            heappush(nums, max(a, b) + 2 * min(a, b))
+            ops += 1
+
+        return ops
