da56f838209cdad7f1118e3ac86a1d768500621d
Anthony Zang <anthonyzang101@gmail.com>
2025-03-12 00:28:58 -0400

    fix file conent for m128

--- my-submissions/m128 v2.py
+++ my-submissions/m128 v2.py
@@ -1,19 +1,25 @@
+# O(n) solution babyyyy
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0 :
             return 0
-        
+
         vals = set(nums)                # O(n) creation, O(1) lookups
 
         maxx = 0
-        for val in vals:                # O(n) looping
-            if val - 1 in vals:         # not start of sequence
-                continue
-            
-            cnter = 1
-            while val + cnter in vals :
-                cnter += 1
+        while vals :
+            left = right = vals.pop()
+            curr = 1
+            while left - 1 in vals :
+                left -= 1
+                curr += 1
+                vals.remove(left)
+            while right + 1 in vals :
+                right += 1
+                curr += 1
+                vals.remove(right)
 
-            maxx = max(maxx, cnter)
+            maxx = max(maxx, curr)
 
         return maxx
