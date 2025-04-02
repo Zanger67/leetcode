4178883b6a3f2454076ccc9754ed32972c7c6d29
Anthony Zang <anthonyzang101@gmail.com>
2025-04-01 20:34:06 -0400

    Optimize daily

---
+++ my-submissions/e2873 v2 optimized O(n).py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        max_n, max_dif, output = 0, 0, 0
+        for n in nums :
+            output = max(output, max_dif * n)
+            max_n = max(max_n, n)
+            max_dif = max(max_dif, max_n - n)
+        return output
